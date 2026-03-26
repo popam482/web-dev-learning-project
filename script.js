1ee7033eb179cbd92d3ccb63ebced387bc6c8309
popam482 <popamaria482@gmail.com>
2026-03-26 23:42:23 +0200

    added dark theme

--- script.js
+++ script.js
@@ -31,3 +31,25 @@ if(form){
         form.reset();
     });
 }
+
+const themeToggle = document.getElementById('themeToggle');
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    themeToggle.textContent = '☀️';
+}
+
+if (themeToggle) {
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+            themeToggle.textContent = '☀️';
+        } else {
+            localStorage.setItem('theme', 'light');
+            themeToggle.textContent = '🌙';
+        }
+    });
+}
